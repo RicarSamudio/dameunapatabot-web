cd76aeb2f1ec72b12929656af71a92456e944b19
root <root@srv1409806.hstgr.cloud>
2026-03-29 05:19:45 +0000

    fix: remove unused ts-expect-error directive

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,7 +4,6 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-// @ts-expect-error Prisma 7 requires URL but types don't show it
 export const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
